0a8ce041ebb0c88e6057d4fe4cd1e80a57df01a9
Janhavi Kulkarni <97089432+Janhavi2802@users.noreply.github.com>
2022-06-11 18:36:39 +0000

    done

--- ActivitySet#1/problem#14.py
+++ ActivitySet#1/problem#14.py
@@ -6,7 +6,7 @@ fhand = open(fh)
 sum = 0
 for line in fhand:
   num = re.findall("[0-9]+",line)
-  length = len(num)
+  length = len(sum)
   for i in range(length):
      sum = sum + int(num[i])
 print(sum)
