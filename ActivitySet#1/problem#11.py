bbe8e5c80a9dfa455de5216c6e0360caf11775a2
Janhavi Kulkarni <97089432+Janhavi2802@users.noreply.github.com>
2022-06-27 16:56:41 +0000

    done

--- ActivitySet#1/problem#11.py
+++ ActivitySet#1/problem#11.py
@@ -5,7 +5,7 @@ count = 0
 fh = open(fname)
 for line in fh:    
     if not line.startswith('From '):continue
-    words = line.split()
+    words = lines.split()
     print(words[1])
-    count = count +1
+   count = count + 1
 print("There were", count, "lines in the file with From as the first word")
